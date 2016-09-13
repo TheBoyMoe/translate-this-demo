c17e52725910fa59b5a59e7364568e32c202cf1c
bill fero <billfero@gmail.com>
2016-09-13 15:39:23 +0100

    impl basic discover ui. Add MS Speech SDK and JNILibs

--- app/src/main/java/com/example/translatethis/common/Constants.java
+++ app/src/main/java/com/example/translatethis/common/Constants.java
@@ -6,4 +6,18 @@ public class Constants {
 
     public static final String LOG_TAG = "LOG";
 
+    // language codes for language spinner
+    public static final String[] LANGUAGE_CODES = {
+            "en-gb", "fr-fr", "de-de", "it-it", "es-es"
+    };
+
+    // languages available for translation - order must match string array order
+//    public static final Language[] LANGUAGES = {
+//            Language.ENGLISH,
+//            Language.FRENCH,
+//            Language.GERMAN,
+//            Language.ITALIAN,
+//            Language.SPANISH
+//    };
+
 }
--- app/src/main/java/com/example/translatethis/main/discover/view/DiscoverFragment.java
+++ app/src/main/java/com/example/translatethis/main/discover/view/DiscoverFragment.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.translatethis.R;
 import com.example.translatethis.main.ContractFragment;
@@ -42,10 +41,7 @@ public class DiscoverFragment extends ContractFragment<DiscoverFragment.Contract
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.content_main, container, false);
-        TextView title = (TextView) view.findViewById(R.id.title);
-        title.setText("Discover Fragment");
-        title.setOnClickListener(titleClick);
+        View view = inflater.inflate(R.layout.content_discover, container, false);
         return view;
     }
 
