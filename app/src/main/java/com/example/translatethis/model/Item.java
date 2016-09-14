6f7249f1617a2d7a42c5983de515c00db19c8caf
bill fero <billfero@gmail.com>
2016-09-14 09:05:14 +0100

    impl dummy item data cache

--- app/src/main/java/com/example/translatethis/model/Item.java
+++ app/src/main/java/com/example/translatethis/model/Item.java
@@ -1,13 +1,34 @@
 package com.example.translatethis.model;
 
 
+import android.content.ContentValues;
+
 public class Item {
 
+    private int mId = -1;
     private String mFromText;
     private String mToString;
     private String mAudioFile;
 
     public Item() {}
+
+    public Item(String fromText, String toString, String audioFile) {
+        mFromText = fromText;
+        mToString = toString;
+        mAudioFile = audioFile;
+    }
+
+    public ContentValues getValues() {
+        return null; // TODO
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
 
     public String getFromText() {
         return mFromText;
---
+++ app/src/main/java/com/example/translatethis/model/data/DummyItemDataCache.java
@@ -0,0 +1,21 @@
+package com.example.translatethis.model.data;
+
+
+import com.example.translatethis.model.Item;
+
+public class DummyItemDataCache {
+
+    public static Item[] dataCache = {
+            new Item("No more switching between apps", "Nicht mehr Umschalten zwischen Anwendungen", ""),
+            new Item("A note can be anything you want it to be", "Eine Notiz kann alles, was Sie wollen, es zu sein", ""),
+            new Item("We're rolling out new features with the iOS 10", "Wir rollen heraus neue Funktionen mit dem iOS- 10", ""),
+            new Item("once you make a note, it’s accessible wherever you go, forever", "wenn Sie eine Notiz zu machen , dann ist es zugänglich , wohin Sie gehen , für immer.", ""),
+            new Item("Capture a note once, and it’s instantly available on all your devices", "Erfassen Sie eine Notiz einmal , und es ist sofort verfügbar auf allen Geräten", ""),
+            new Item("Whether it’s text, images or documents, you keep things for a reason.", "Ob es sich um Text , Bilder oder Dokumente ist , halten Sie die Dinge aus einem Grund", ""),
+            new Item("You can even search for handwritten words buried deep within your notes", "Sie können sogar für handgeschriebene Wörter suchen tief in Ihre Notizen begraben", ""),
+            new Item("Capture life’s little moments and share them", "Capture life’s little moments and share them", ""),
+            new Item("Share big ideas by collaborating seamlessly with co-workers", "teilen große Ideen , indem sie nahtlos mit Kollegen zusammenarbeiten", ""),
+            new Item("Evernote makes sure the notes you’ve saved are easy to find", "Evernote stellt sicher, dass die Noten, die Sie gespeichert haben, sind leicht zu finden", ""),
+    };
+
+}
